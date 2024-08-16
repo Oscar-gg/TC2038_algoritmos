1141546dabaf48c9d06b250683232afb3e503149
Alejandra Coeto <a01285221@tec.mx>
2024-08-15 20:49:58 -0700

    Added comments

--- a01178076_Act1.1/main.cpp
+++ a01178076_Act1.1/main.cpp
@@ -1,30 +1,22 @@
-/* Instrucciones */
+// Oscar Arreola (A01178076) y Alejandra Coeto (A01285221)
+// Última modificación: Jueves 15 de Agosto
+// Programa para probar la implementación del algoritmo MergeSort
 
-// Utilizando la técnica de programación de "divide y vencerás", escribe en equipos de máximo dos personas,
-// en C++ un programa que implemente el algoritmo de MergeSort.
-
-// El programa recibe un numero entero N, seguido de N valores reales (double, uno en cada línea) por la entrada estándar.
-// La salida del programa es una lista de los N valores ordenados de Mayor a menor
-// Tu programa debe ser llamado main.cpp
-
-// Sube un archivo ZIP que se llame <tumatricula_Act1.1>.ZIP
-//      El archivo ZIP contiene una carpeta (folder) llamada <tumatricula_Act1.1>, y dentro se encuentra el archivo "main.cpp"
-//      Es posible tener más archivos dentro de la carpeta
 
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 // Complejidad temporal: O(r - l + 1)
 // Complejidad espacial: O(r - l + 1)
-void merge(vector<double> &arreglo, int l, int m, int r, bool descendente=false){
+// Función merge para combinar los sub-arreglos
+void merge(vector<double> &arreglo, int l, int m, int r, bool descendente=false) {
     vector<double> temporal;
     int p1 = l, p2 = m+1;
 
-    while (p1 <= m || p2 <= r){
-        if (p1 <= m && p2 <= r){
-            if ((arreglo[p1] < arreglo[p2]) ^ descendente){
+    while (p1 <= m || p2 <= r) {
+        if (p1 <= m && p2 <= r) {
+            if ((arreglo[p1] < arreglo[p2]) ^ descendente) {
                 temporal.push_back(arreglo[p1++]);
             } else {
                 temporal.push_back(arreglo[p2++]);
@@ -36,7 +28,7 @@ void merge(vector<double> &arreglo, int l, int m, int r, bool descendente=false)
         }
     }
 
-    for(int i = l; i <= r; i++){
+    for(int i = l; i <= r; i++) {
         arreglo[i] = temporal[i-l];
     }
 
@@ -45,8 +37,9 @@ void merge(vector<double> &arreglo, int l, int m, int r, bool descendente=false)
 // O(Nlog(N)), donde N es el tamaño del arreglo inicial
 // Cada operacion de merge toma O(N) tiempo
 // Y esta operacion se realiza log(N) veces
-void mergeSort(vector<double> &arreglo, int l, int r, bool descendente=false){
-    if(l < r){
+// Función merge sort para dividir el problema en sub-problemas
+void mergeSort(vector<double> &arreglo, int l, int r, bool descendente=false) {
+    if(l < r) {
         int m = l + (r - l) / 2;
 
         mergeSort(arreglo, l, m, descendente);
@@ -56,7 +49,8 @@ void mergeSort(vector<double> &arreglo, int l, int r, bool descendente=false){
     }
 }
 
-int main(){
+// Función principal para recibir las entradas e imprimir el resultado
+int main() {
     cout << "Ingrese N: ";
     int n;
     cin >> n;
@@ -70,7 +64,7 @@ int main(){
     vector<double> arreglo(n);
     
     // Llenar el arreglo de valores
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++) {
         cout << "Ingrese el valor " << i+1 << ": ";
         cin >> arreglo[i];
         cout << "\n";
@@ -81,7 +75,7 @@ int main(){
 
     // Mostrar el resultado
     cout << "\n\nResultado: ";
-    for(double elemento : arreglo){
+    for(double elemento : arreglo) {
         cout << elemento << " ";
     }
 
